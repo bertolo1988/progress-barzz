d683be5b5b0ee73f907e0b11df407249f17e0bcf
Tiago Bértolo <tiagobertolo@gmail.com>
2020-11-26 00:45:52 +0000

    Commit eslint and prettier config.

--- demo.js
+++ demo.js
@@ -4,10 +4,10 @@ const HASHES = 20;
 const timeout = 200;
 
 function printTick() {
-	console.log(Progress.tick());
+  console.log(Progress.tick());
 }
 
 Progress.init(HASHES);
 for (let i = 0; i < HASHES; i++) {
-	setTimeout(printTick, timeout * i);
+  setTimeout(printTick, timeout * i);
 }
